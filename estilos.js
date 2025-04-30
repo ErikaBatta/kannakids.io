054508b757c75bf6f424b0cb368067d40d7c09a4
ErikaBatta <ekabatta@gmail.com>
2025-04-30 17:45:52 -0500

    Add files via upload

---
+++ estilos.js
@@ -0,0 +1,14 @@
+let formulario = document.getElementById('miformulario');
+
+// Paso 2: Definir qué hacer cuando se envía
+function manejarEnvio(evento) {
+  evento.preventDefault(); // Previene que se recargue la página
+  alert("Los datos fueron enviados correctamente.");
+}
+
+// Paso 3: Conectar el evento con la función
+formulario.addEventListener('submit', manejarEnvio);
+
+function vaciar(){
+  this.reset();
+}
